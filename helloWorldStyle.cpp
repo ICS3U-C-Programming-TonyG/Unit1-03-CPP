26dd82594233f8249526bd859487b4dfcc9fe8a1
TONY GAO <TONY.GAO@STU.OCSB.CA>
2025-02-14 18:09:33 +0000

    CPP

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2025 Tony Gao All Rights Authorized
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+    std::cout << "C++ is alot more complicated than python" << std::endl;
+}
